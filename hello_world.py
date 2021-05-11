fdc058f6cfeef3043b73d8d8bf040527b0569f5d
Ajay Hooda <AjayHooda@tsys.com>
2021-05-11 11:09:59 -0400

    enhanced check to ignore special characters & case

--- hello_world.py
+++ hello_world.py
@@ -1,12 +1,11 @@
-#print("Hello world")
-
 import PySimpleGUI as sg
 def main():
     sg.theme('DarkAmber')   # Add a touch of color
     # All the stuff inside your window.
     layout = [  [sg.Text('Want to know if a marine animal is in extreme danger of endangerment?')],
-                [sg.Text('Wrtie the name of the marine animal you want to know about!'), sg.InputText(key='box')],
-                [sg.Button('Ok'), sg.Button('Close Window')]]
+                [sg.Text('Name of marine animal:'), sg.InputText(key='box')],
+                [sg.Button('Ok'), sg.Button('Close Window')],
+                [sg.Button('Submit', visible=False, bind_return_key=True)]]
 
 
 
@@ -26,14 +25,18 @@ def main():
     
     window.close()
 
-def my_function(animalName):
-    b=('Vaquita', 'Whale Sharks', 'Hawksbill Sea Turtle', 'Sea Otter')
-    print(animalName)
-    if animalName in b:
-        print(animalName + ' is an extremely endangered species!')
+b=('VAQUITA', 'WHALESHARKS', 'HAWKSBILLSEATURTLE', 'SEAOTTER') # globallist of animals in uppercase without special characters
+
+def my_function(animal_name):
+    #print(''.join(e for e in animal_name if e.isalnum()))
+    #loop through each character in animal_name and add them to blank if its a alphanumeric character to remove any other characters
+    alphanum = ''.join(e for e in animal_name if e.isalnum())
+    # check against uppercase of what the user inputs to match against our master list which is in upper case
+    if alphanum.upper() in b:
+        print(alphanum.upper()  + ' is an extremely endangered species!')
         return True
     else:
-        print(animalName + ' is not extremely endangered!')
+        print(alphanum.upper()  + ' is not extremely endangered!')
         return False
 
 main()
