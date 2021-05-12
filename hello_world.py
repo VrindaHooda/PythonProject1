d1338ac57288e1ab41278aa0c275c7e0d451d307
vrindahooda <vrindahooda@gmail.com>
2021-05-11 21:02:25 -0400

    renamed function

--- hello_world.py
+++ hello_world.py
@@ -1,39 +1,29 @@
-#print("Hello world")
-
 import PySimpleGUI as sg
 def main():
-    sg.theme('DarkAmber')   # Add a touch of color
-    # All the stuff inside your window.
-    layout = [  [sg.Text('Want to know if a marine animal is in extreme danger of endangerment?')],
-                [sg.Text('Wrtie the name of the marine animal you want to know about!'), sg.InputText(key='box')],
+    sg.theme('LightBlue1')   # Theme color for my UI
+    # All the things inside my window.
+    layout = [  [sg.Text('Want to know if a marine animal is in the top ten most endangered marine animals list?')],
+                [sg.Text('Write the name of the marine animal and we will let you know!'), sg.InputText(key='box')],
                 [sg.Button('Ok'), sg.Button('Close Window')]]
 
-
-
     window = sg.Window('Endangered Animals', layout).Finalize()
-    #sg.Window(title="Hello World", layout=[[]], margins=(100, 50)).read()
-    while True:
+    while True: # Loop
         event, values = window.read()
-        if event in (None, 'Close Window'): # if user closes window or clicks cancel
+        if event in (None, 'Close Window'): # if user closes window or clicks "close window"
             break
-        print('You entered in the box:')
-        print(values['box'])  # get the content of multiline via its unique key
-        if my_function(values['box']):
-            sg.popup(values['box'], ' is endangered')
-        else:
-            sg.popup(values['box'], ' is not endangered')    
+        if my_function(values['box']): # if my_function is true
+            sg.popup(values['box'], 'is in the top ten most endangered marine animals list!')
+        else: # if my function is false
+            sg.popup(values['box'], 'is not in the top ten most endangered marine animals list!')    
         
     
     window.close()
-
-def my_function(animalName):
-    b=('Vaquita', 'Whale Sharks', 'Hawksbill Sea Turtle', 'Sea Otter')
-    print(animalName)
-    if animalName in b:
-        print(animalName + ' is an extremely endangered species!')
+    
+    endangeredList = ('Vaquita', 'Whale Shark', 'Hawksbill Sea Turtle', 'Sea Otter', 'Blue Whale', 'River Dolphin', 'Florida Manatee', 'Galapagos Penguin', 'Hawaiian Monk Seal', 'Kemp\'s Ridley Sea Turtle')
+def check_endangered_animal(animalName):
+    if animalName in endangeredList: # if user input matches my list
         return True
-    else:
-        print(animalName + ' is not extremely endangered!')
+    else: # if user input does not match my list
         return False
 
 main()
